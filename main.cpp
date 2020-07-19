45d7c31d6253e427cdbcafbbd0b1564ede07d172
webturing <zj@webturing.com>
2020-07-19 15:25:36 +0800

    cpp空模板

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by ceo on 2020/7/19.
+//
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int a[10] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
+
+    
+    for (auto &e:a) {
+        cout << e << " ";
+    }
+    return 0;
+}
