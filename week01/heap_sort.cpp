4b6c1b7724b42193da0fcde58ef3eacfc5a416e8
webturing <zj@webturing.com>
2020-07-26 10:13:28 +0800

    week01 lec02高级排序和查找

---
+++ week01/heap_sort.cpp
@@ -0,0 +1,25 @@
+//
+// Created by ceo on 2020/7/19.
+//
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int a[10] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
+//    make_heap(a, a + 10,greater<int>());
+//    for (int i = 0; i < 10; i++) {
+//
+//        cout<<a[0]<<" ";  pop_heap(a, a + 10 - i,greater<int>());
+//    }
+    priority_queue<int> Q;
+    for(int i:a)Q.push(-i);
+    while(Q.size()){
+        cout<< -Q.top()<<" ";
+        Q.pop();
+    }
+    cout<<endl;
+    return 0;
+}
--- week01/quick_sort.cpp
+++ week01/quick_sort.cpp
@@ -6,44 +6,44 @@
 
 using namespace std;
 
-//int Partition(int *a, int left, int right) {
+int Partition(int *a, int left, int right) {
+    int piv = a[left];
+    int i = left;
+    int j = right;
+    while (i < j) {
+        while (j > i && a[j] >= piv)j--;
+        while (i < j && a[i] <= piv)i++;
+        if (i >= j)break;
+        swap(a[i], a[j]);
+    }
+    swap(a[left], a[i-1]);
+    return i-1;
+
+
+}
+void quick_sort(int *a, int left, int right) {
+    if (left >= right) return;
+    int i = Partition(a, left, right);//把数组分为三部分 a[left]位于中间 左边都比它小，右边都比它大
+    quick_sort(a, left, i - 1);//
+    quick_sort(a, i-1, right);
+    return;
+}
+
+//void quick_sort(int *a, int left, int right) {
+//    if (left >= right)return;
+//
 //    int piv = a[left];
-//    int i = left;
-//    int j = right;
+//    int i = left, j = right;
 //    while (i < j) {
-//        while (j > i && a[j] >= piv)j--;
-//        while (i < j && a[i] <= piv)i++;
-//        if (i >= j)break;
+//        while (j > i && a[j] >= piv)--j;
+//        while (j > i && a[i] <= piv)++i;
 //        swap(a[i], a[j]);
 //    }
 //    swap(a[left], a[i]);
-//    return i;
-//
+//    quick_sort(a, left, i-1);
+//    quick_sort(a, i+ 1, right);
 //
 //}
-//void quick_sort(int *a, int left, int right) {
-//    if (left >= right) return;
-//    int i = Partition(a, left, right);//把数组分为三部分 a[left]位于中间 左边都比它小，右边都比它大
-//    quick_sort(a, left, i - 1);//
-//    quick_sort(a, i-1, right);
-//    return;
-//}
-
-void quick_sort(int *a, int left, int right) {
-    if (left >= right)return;
-
-    int piv = a[left];
-    int i = left, j = right;
-    while (i < j) {
-        while (j > i && a[j] >= piv)--j;
-        while (j > i && a[i] <= piv)++i;
-        swap(a[i], a[j]);
-    }
-    swap(a[left], a[i]);
-    quick_sort(a, left, i-1);
-    quick_sort(a, i+ 1, right);
-
-}
 
 int main() {
     int a[10] = {3, 1, 5, 7, 9, 2, 4, 6, 8, 0};
