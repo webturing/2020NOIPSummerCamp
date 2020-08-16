a6bf07ede9a910861181a86f1a7532eaddd45006
webturing <zj@webturing.com>
2020-08-16 13:51:02 +0800

    IO更新 第五周图和树

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<iterator>
+#include<string>
+#include<cmath>
+#include<cstring>
+#include<iomanip>
+#include<utility>
+#include<vector>
+#include<map>
+#include<queue>
+#include<set>
+
 using namespace std;
 
 using ll=long long;
 #define endl '\n'
 #define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
 #define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
-template<typename T=int>
-inline T read() {
-    T x;
-    cin >> x;
-    return x;
-}
+
 
 int main() {
     ios::sync_with_stdio(false);
