3ce6af60b7af9c0ce66f4d3975764ebf714da547
webturing <zj@webturing.com>
2020-08-23 16:23:58 +0800

    week06graph

--- week06graph/group.cpp
+++ week06graph/group.cpp
@@ -1,33 +1,67 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#define MAXINT 6
+
 using namespace std;
-int U[110], V[110], W[110];
-int Next[110], First[110];
 
-int n, m;
-
-void addedge(int i)
+//声明一个二维数组,C[i][j]存储的是点i到点j的边的权值,如果不可达,则用1000表示
+//借此二维数组来表示一个连通带权图
+int c[MAXINT][MAXINT]={{1000,6,1,5,1000,1000},{6,1000,5,1000,3,1000},{1,5,1000,5,6,4},{5,1000,5,1000,1000,2},{1000,3,6,1000,1000,6},{1000,1000,4,2,6,1000}};
+void Prim(int n)
 {
-    Next[i] = First[U[i]];
-    First[U[i]] = i;
-}
+    int lowcost[MAXINT];//存储S中到达对应的其它各点的最小权值分别是多少
+    int closest[MAXINT];//closest[]数组保存的是未在S中的点所到达S中包含的最近的点是哪一个,如:closest[i]=1表示i最靠近的S中的点是1
+    bool s[MAXINT];//bool型变量的S数组表示i是否已经包括在S中
+    int i,k;
+    s[0]=true;//从第一个结点开始寻找,扩展
+    for(i=1;i<=n;i++)//简单初始化
+    {
+        lowcost[i]=c[0][i];
+        closest[i]=0;//现在所有的点对应的已经在S中的最近的点是1
+        s[i]=false;
+    }
+    cout<<"0->";
+    for(i=0;i<n;i++)
+    {
+        int min=1000;//最小值,设大一点的值,后面用来记录lowcost数组中的最小值
+        int j=1;
+        for(k=1;k<=n;k++)//寻找lowcost中的最小值
+        {
+            if((lowcost[k]<min)&&(!s[k]))
+            {
+                min=lowcost[k];j=k;
+            }
+        }
+        cout<<j<<" "<<"->";
+        s[j]=true;//添加点j到集合S中
+        for(k=1;k<=n;k++)//因为新加入了j点,所以要查找新加入的j点到未在S中的点K中的权值是不是可以因此更小
+        {
+            if((c[j][k]<lowcost[k])&&(!s[k])){lowcost[k]=c[j][k];closest[k]=j;}
+        }
+    }
 
+}
 int main()
 {
-    freopen("te.in", "r", stdin);
-    cin >> n >> m;
-    memset(First, -1, sizeof(First));
-    memset(Next, -1, sizeof(Next));
-    for (int i = 1;i <= m;i++)
+
+    //输入初始化数组
+
+    cout<<"请输入初始权值数组:"<<endl;
+    for(int i=0;i<MAXINT;i++)
     {
-        cin >> U[i] >> V[i] >> W[i];
-        addedge(i);
-    }
-    for (int i = 1;i <= n;i++)
-    {
-        cout << i << ": \n";
-        for (int e = First[i];e != -1;e = Next[e])
-            cout << U[e] << ' ' << V[e] << endl;
+        for(int j=0;j<MAXINT;j++)
+        {
+            //    cout<<"please enter c["<<i<<"]["<<j<<"]:";
+            //    cin>>c[i][j];
+            cout<<c[i][j]<<"\t";
+            //    cout<<endl;
+        }
+        cout<<endl;
     }
 
+    Prim(MAXINT-1);
     return 0;
+
+
+
+
 }
--- week06graph/outline.md
+++ week06graph/outline.md
@@ -1,6 +1,20 @@
 #day05
 # 图
-* 单源最短路
 * 欧拉图
-* 最小生成树
+
 * 拓扑排序
+
+  
+
+* 单源最短路 第6章,n个点 m个边 m>>n   floyed p148 O(n^3)
+
+  * dijkstra  O(n^2)/O(nlogn) 
+
+* 最小生成树 第8章
+
+  * kruskal  mlog(m)+o(n)
+  * prim  Om^2)/mlog(m)
+
+  
+
+  
